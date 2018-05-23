4a8e258e34e8d7fe5179494a5690c369956fe20d
linda <nmlinda97@gmail.com>
2018-05-23 08:36:24 +0700

    last

--- application/models/Auth_model.php
+++ application/models/Auth_model.php
@@ -3,6 +3,10 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Auth_model extends CI_Model {
 
+    public function __construct(){
+        parent::__construct();
+        $this->load->database();
+    }
     public function register($data)
     {
         return $this->db->insert('users', $data);
